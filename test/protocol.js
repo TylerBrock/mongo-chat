6cfe72577560332407a95f35bfd333020803dfec
Tyler Brock <tyler.brock@gmail.com>
2013-12-29 20:22:50 -0500

    updates, tests for nick validity

--- test/protocol.js
+++ test/protocol.js
@@ -40,4 +40,58 @@ describe('Protocol', function() {
       assert.deepEqual(protocol.decode(data), message);
     })
   })
+
+  describe('validation', function() {
+    describe('nick', function() {
+      it('should reject nicks longer than 9 characters', function() {
+        var nick = "TylerBrock";
+        assert(!protocol.validation.nick.test(nick));
+      })
+
+      it('should reject nicks starting with a number', function() {
+        var nick = "6Tyler";
+        assert(!protocol.validation.nick.test(nick));
+      })
+
+      it('should reject nicks starting with a special character', function() {
+        var nick = "^Tyler";
+        assert(!protocol.validation.nick.test(nick));
+      })
+
+      it('should accept nicks with carets', function() {
+        var nick = "Ty^ler^";
+        assert(protocol.validation.nick.test(nick));
+      })
+
+      it('should accept nicks with braces', function() {
+        var nick = "Ty{ler}";
+        assert(protocol.validation.nick.test(nick));
+      })
+
+      it('should accept nicks with brackets', function() {
+        var nick = "Ty[ler]";
+        assert(protocol.validation.nick.test(nick));
+      })
+
+      it('should accept nicks with ticks', function() {
+        var nick = "Ty`ler`";
+        assert(protocol.validation.nick.test(nick));
+      })
+
+      it('should accept nicks with pipes', function() {
+        var nick = "Ty|ler|";
+        assert(protocol.validation.nick.test(nick));
+      })
+
+      it('should accept nicks with dashes', function() {
+        var nick = "Ty-ler-";
+        assert(protocol.validation.nick.test(nick));
+      })
+
+      it('should accept nicks with numbers', function() {
+        var nick = "Ty13r";
+        assert(protocol.validation.nick.test(nick));
+      })
+    })
+  })
 })
